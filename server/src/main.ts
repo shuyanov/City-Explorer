761609f9ececdcaa1aa73e5b9bc853041be1b725
SunM1sty <mis7akes@mail.ru>
2023-10-13 14:30:17 +0300

    fix: resolve errors of nest modules

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// !
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // !
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  // обратите внимание на порт
+  await app.listen(3001);
 }
 bootstrap();
