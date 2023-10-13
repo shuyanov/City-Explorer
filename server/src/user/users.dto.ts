ac7b64d03ae314f669face908b200f5abc18cdd4
Uzhastin-Nikita <fosfor20@mail.ru>
2023-10-14 00:45:09 +0300

    feat(users): add name to dto

--- server/src/user/users.dto.ts
+++ server/src/user/users.dto.ts
@@ -1,10 +1,14 @@
 export class CreateUserDto {
+  name: string;
+
   user_photos?: number[];
 
   user_achievements?: number[];
 }
 
 export class UpdateUserDto {
+  name: string;
+
   user_photos?: number[];
 
   user_achievements?: number[];
