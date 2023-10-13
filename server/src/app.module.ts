c2d7888ffda5b9982df5c8be049b811466f3bf03
SunM1sty <mis7akes@mail.ru>
2023-10-13 15:44:19 +0300

    feat: add main route

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './user/users.module';
 
 @Module({
   imports: [UsersModule],
-  controllers: [],
-  providers: [PrismaService]
+  controllers: [AppController],
+  providers: [PrismaService, AppService],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// !
-import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // !
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
-
-  // обратите внимание на порт
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
