3bcfce8e58f534d0d805cb3a4155f408888b429b
Uzhastin-Nikita <fosfor20@mail.ru>
2023-10-13 19:26:42 +0300

    refactor(achievements): add achievements separate type file

---
+++ server/src/achievement/achievements.types.ts
@@ -0,0 +1,13 @@
+export const achievement = {
+  ID: 'id',
+  POST_ACHIEVEMENTS_SUMMARY: 'Create a new achievement',
+  POST_ACHIEVEMENTS_DESCRIPTION:
+    'The achievement has been successfully created.',
+  GET_ACHIEVEMENTS_SUMMARY: 'Get all achievements',
+  GET_ACHIEVEMENTS_DESCRIPTION: 'The achievements has been get.',
+  GETID_ACHIEVEMENT_SUMMARY: 'Get a achievement by id',
+  GETID_ACHIEVEMENT_DESCRIPTION: 'The achievement has been get.',
+  DELETE_ACHIEVEMENT_SUMMARY: 'Delete a achievement',
+  DELETE_ACHIEVEMENT_DESCRIPTION:
+    'The achievement has been successfully deleted.',
+};
