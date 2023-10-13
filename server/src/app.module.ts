bd677d0c6cb5060ea659877341afd5192c3297f9
Uzhastin-Nikita <fosfor20@mail.ru>
2023-10-14 00:22:40 +0300

    feat(app): add achievementPhoto to main model

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,17 @@ import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './user/users.module';
 import { PhotosModule } from './photo/photos.module';
 import { AchievementsModule } from './achievement';
+import { AchievementsPhotoModule } from './achievementPhoto';
 import { SwaggerDocsModule } from './swagger/swagger.module';
 
 @Module({
-  imports: [UsersModule, AchievementsModule, PhotosModule, SwaggerDocsModule],
+  imports: [
+    UsersModule,
+    AchievementsModule,
+    PhotosModule,
+    SwaggerDocsModule,
+    AchievementsPhotoModule,
+  ],
   controllers: [AppController],
   providers: [PrismaService, AppService],
 })
