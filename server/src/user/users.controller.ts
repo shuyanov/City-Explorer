edd2d2c3891dfe7e9cb6b6644e685cf004fafc72
SunM1sty <mis7akes@mail.ru>
2023-10-13 14:06:14 +0300

    fix: resolve conflicts after rebasing

---
+++ server/src/user/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  async getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+}
