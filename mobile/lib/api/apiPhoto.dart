275b3fb7a6ed7a354b894e7d94c8b4f1975d72a5
HubOl01 <kuloleg54@gmail.com>
2023-10-14 03:05:37 +0300

    fixed api photos

--- mobile/lib/api/apiPhoto.dart
+++ mobile/lib/api/apiPhoto.dart
@@ -5,7 +5,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:test_hackaton/Model/PhotoModel.dart';
 
 
-dynamic photos;
+List<Photo> photos = [];
   Future getPhotos() async {
     final url = Uri.parse(
         "${dotenv.env['DB_URL']}/photos");
@@ -18,7 +18,7 @@ dynamic photos;
        photos = photoFromJson(response.body);
          
           print(
-              "Successed write ===>>> getPhotos > ${photos.toJson().toString()}");
+              "Successed write ===>>> getPhotos > ${photos.toString()}");
 
           print("getPhotos: ${response.statusCode}");
       print('Photos успешно отправлены на сервер');
--- mobile/lib/pages/mapPage.dart
+++ mobile/lib/pages/mapPage.dart
@@ -145,7 +145,19 @@ class _MapPageState extends State<MapPage> {
         ),
       ),
     ];
-    for(int i = 0; i < photos.length; i++){
+   
+    
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            mapController.move(LatLng(56.9972, 40.9714), 10);
+          },
+          child: Icon(Icons.location_history),
+        ),
+        body: FutureBuilder(
+          future: getPhotos(),
+          builder: (context, snapshot) {
+             for(int i = 0; i < photos.length; i++){
          listMarker.add(Marker (
         anchorPos: AnchorPos.align(AnchorAlign.top),
         rotateAlignment: AlignmentGeometry.lerp(
@@ -156,7 +168,7 @@ class _MapPageState extends State<MapPage> {
         width: _markerSize,
         height: _markerSize,
 
-        point: LatLng(photos.latitude, photos.longitude),
+        point: LatLng(photos[i].latitude, photos[i].longitude),
         builder: (ctx) => InkWell(
           onTap: () async {
             return await showDialog(
@@ -188,7 +200,7 @@ class _MapPageState extends State<MapPage> {
             ),
             child: ClipRRect(
               child: Image.network(
-                "https://fasadnaya-kraska.ru/upload/articles/ru/0_65518000_1625121805_img.jpg",
+                photos[i].urlImage,
                 fit: BoxFit.cover,
               ),
               borderRadius: BorderRadius.circular(_markerSize > 100
@@ -201,141 +213,135 @@ class _MapPageState extends State<MapPage> {
         ),
       ));
     }
-    
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            mapController.move(LatLng(56.9972, 40.9714), 10);
-          },
-          child: Icon(Icons.location_history),
-        ),
-        body: FlutterMap(
-          mapController: mapController,
-          options: MapOptions(
-            center: LatLng(56.9972, 40.9714),
-            minZoom: 0,
-            zoom: 10,
-            maxZoom: 18.49,
-            onTap: (p, s) async {
-              location = await Geocoder.local.findAddressesFromCoordinates(
-                  new Coordinates(s.latitude, s.longitude));
-
-              setState(() {
-                point = s;
-                print(s);
-              });
-
-              print(
-                  "${location.first.countryName} - ${location.first.addressLine}");
-            },
-            onPositionChanged: (position, hasGesture) {
-              if (hasGesture) {
-                _updateMarkerSize(position.zoom!);
-              }
-            },
-          ),
-          nonRotatedChildren: [
-            // RichAttributionWidget(
-            //   attributions: [
-            //     TextSourceAttribution(
-            //       'OpenStreetMap contributors',
-            //       onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-            //     ),
-            //   ],
-            // ),
-          ],
-          children: [
-            TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                // urlTemplate:'https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
-
-                userAgentPackageName: 'com.example.app',
-                subdomains: ['a', 'b', 'c'],
-                maxNativeZoom: 20),
-            MarkerLayer(markers: listMarker
-                // [
-                //       Marker(
-
-                //         anchorPos: AnchorPos.align(AnchorAlign.top),
-                //         rotateAlignment: AlignmentGeometry.lerp(
-                //             Alignment.bottomCenter, Alignment.bottomCenter, 0),
-                //         rotate: true,
-                //         // width: 300.0,
-                //         // height: 300.0,
-                //         width: _markerSize,
-                //         height: _markerSize,
-
-                //         point: point,
-                //         builder: (ctx) => InkWell(
-                //           child: Container(
-                //             width: _markerSize,
-                //             height: _markerSize,
-                //             decoration: ShapeDecoration(
-                //               color: Colors.white,
-                //               shape: _markerSize > 50
-                //                   ? CustomMarker(size: _markerSize)
-                //                   : _markerSize == 50
-                //                       ? RoundedRectangleBorder(
-                //                           borderRadius: BorderRadius.circular(10))
-                //                       : CircleBorder(),
-                //               shadows: [
-                //                 BoxShadow(
-                //                     color: Colors.grey,
-                //                     // blurRadius: 1.0,
-                //                     offset: Offset(2, 2)),
-                //               ],
-                //             ),
-                //             child: ClipRRect(
-                //                 child: Image.network(
-                //               "https://fasadnaya-kraska.ru/upload/articles/ru/0_65518000_1625121805_img.jpg",
-                //               fit: BoxFit.cover,
-                //             ),
-                //             borderRadius: BorderRadius.circular(_markerSize > 100 ? 40 : _markerSize == 50 ? 10 : 20),),
-                //           ),
-                //         ),
-                //       ),
-                //       Marker(
-                //         anchorPos: AnchorPos.align(AnchorAlign.top),
-                //         rotateAlignment: AlignmentGeometry.lerp(
-                //             Alignment.bottomCenter, Alignment.bottomCenter, 0),
-                //         rotate: true,
-                //         // width: 300.0,
-                //         // height: 300.0,
-                //         width: _markerSize,
-                //         height: _markerSize,
-
-                //         point: LatLng(56.925334, 40.973885),
-                //         builder: (ctx) => InkWell(
-                //           child: Container(
-                //             width: _markerSize,
-                //             height: _markerSize,
-                //             decoration: ShapeDecoration(
-                //               color: Colors.white,
-                //               shape: _markerSize > 50
-                //                   ? CustomMarker(size: _markerSize)
-                //                   : _markerSize == 50
-                //                       ? RoundedRectangleBorder(
-                //                           borderRadius: BorderRadius.circular(10))
-                //                       : CircleBorder(),
-                //               shadows: [
-                //                 BoxShadow(
-                //                     color: Colors.grey,
-                //                     // blurRadius: 1.0,
-                //                     offset: Offset(2, 2)),
-                //               ],
-                //             ),
-                //             child: ClipRRect(
-                //                 child: Image.network(
-                //               "https://cdn1.ozone.ru/s3/multimedia-0/6108964908.jpg",
-                //               fit: BoxFit.cover,
-                //             ),
-                //             borderRadius: BorderRadius.circular(_markerSize > 100 ? 40 : _markerSize == 50 ? 10 : 20),),
-                //           ),
-                //         ),
-                //       ),
-                //     ],
-                )
-          ],
+            return FlutterMap(
+              mapController: mapController,
+              options: MapOptions(
+                center: LatLng(56.9972, 40.9714),
+                minZoom: 0,
+                zoom: 10,
+                maxZoom: 18.49,
+                onTap: (p, s) async {
+                  location = await Geocoder.local.findAddressesFromCoordinates(
+                      new Coordinates(s.latitude, s.longitude));
+        
+                  setState(() {
+                    point = s;
+                    print(s);
+                  });
+        
+                  print(
+                      "${location.first.countryName} - ${location.first.addressLine}");
+                },
+                onPositionChanged: (position, hasGesture) {
+                  if (hasGesture) {
+                    _updateMarkerSize(position.zoom!);
+                  }
+                },
+              ),
+              nonRotatedChildren: [
+                // RichAttributionWidget(
+                //   attributions: [
+                //     TextSourceAttribution(
+                //       'OpenStreetMap contributors',
+                //       onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+                //     ),
+                //   ],
+                // ),
+              ],
+              children: [
+                TileLayer(
+                    urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    // urlTemplate:'https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+        
+                    userAgentPackageName: 'com.example.app',
+                    subdomains: ['a', 'b', 'c'],
+                    maxNativeZoom: 20),
+                MarkerLayer(markers: listMarker
+                    // [
+                    //       Marker(
+        
+                    //         anchorPos: AnchorPos.align(AnchorAlign.top),
+                    //         rotateAlignment: AlignmentGeometry.lerp(
+                    //             Alignment.bottomCenter, Alignment.bottomCenter, 0),
+                    //         rotate: true,
+                    //         // width: 300.0,
+                    //         // height: 300.0,
+                    //         width: _markerSize,
+                    //         height: _markerSize,
+        
+                    //         point: point,
+                    //         builder: (ctx) => InkWell(
+                    //           child: Container(
+                    //             width: _markerSize,
+                    //             height: _markerSize,
+                    //             decoration: ShapeDecoration(
+                    //               color: Colors.white,
+                    //               shape: _markerSize > 50
+                    //                   ? CustomMarker(size: _markerSize)
+                    //                   : _markerSize == 50
+                    //                       ? RoundedRectangleBorder(
+                    //                           borderRadius: BorderRadius.circular(10))
+                    //                       : CircleBorder(),
+                    //               shadows: [
+                    //                 BoxShadow(
+                    //                     color: Colors.grey,
+                    //                     // blurRadius: 1.0,
+                    //                     offset: Offset(2, 2)),
+                    //               ],
+                    //             ),
+                    //             child: ClipRRect(
+                    //                 child: Image.network(
+                    //               "https://fasadnaya-kraska.ru/upload/articles/ru/0_65518000_1625121805_img.jpg",
+                    //               fit: BoxFit.cover,
+                    //             ),
+                    //             borderRadius: BorderRadius.circular(_markerSize > 100 ? 40 : _markerSize == 50 ? 10 : 20),),
+                    //           ),
+                    //         ),
+                    //       ),
+                    //       Marker(
+                    //         anchorPos: AnchorPos.align(AnchorAlign.top),
+                    //         rotateAlignment: AlignmentGeometry.lerp(
+                    //             Alignment.bottomCenter, Alignment.bottomCenter, 0),
+                    //         rotate: true,
+                    //         // width: 300.0,
+                    //         // height: 300.0,
+                    //         width: _markerSize,
+                    //         height: _markerSize,
+        
+                    //         point: LatLng(56.925334, 40.973885),
+                    //         builder: (ctx) => InkWell(
+                    //           child: Container(
+                    //             width: _markerSize,
+                    //             height: _markerSize,
+                    //             decoration: ShapeDecoration(
+                    //               color: Colors.white,
+                    //               shape: _markerSize > 50
+                    //                   ? CustomMarker(size: _markerSize)
+                    //                   : _markerSize == 50
+                    //                       ? RoundedRectangleBorder(
+                    //                           borderRadius: BorderRadius.circular(10))
+                    //                       : CircleBorder(),
+                    //               shadows: [
+                    //                 BoxShadow(
+                    //                     color: Colors.grey,
+                    //                     // blurRadius: 1.0,
+                    //                     offset: Offset(2, 2)),
+                    //               ],
+                    //             ),
+                    //             child: ClipRRect(
+                    //                 child: Image.network(
+                    //               "https://cdn1.ozone.ru/s3/multimedia-0/6108964908.jpg",
+                    //               fit: BoxFit.cover,
+                    //             ),
+                    //             borderRadius: BorderRadius.circular(_markerSize > 100 ? 40 : _markerSize == 50 ? 10 : 20),),
+                    //           ),
+                    //         ),
+                    //       ),
+                    //     ],
+                    )
+              ],
+            );
+          }
         ));
   }
 }
