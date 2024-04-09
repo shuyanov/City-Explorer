5bbbd02304e225873c1c73d564858f6dfded9a78
Uzhastin-Nikita <fosfor20@mail.ru>
2024-04-09 23:39:07 +0300

    refactor(mobile): formate code

--- mobile/lib/api/apiPhoto.dart
+++ mobile/lib/api/apiPhoto.dart
@@ -4,29 +4,25 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:test_hackaton/Model/PhotoModel.dart';
 
-
 dynamic photos;
-  Future getPhotos() async {
-    final url = Uri.parse(
-        "${dotenv.env['DB_URL']}/photos");
+Future getPhotos() async {
+  final url = Uri.parse("${dotenv.env['DB_URL']}/photos");
 
-    final response = await http.get(
-      url,
-    );
-    try{
+  final response = await http.get(
+    url,
+  );
+  try {
     if (response.statusCode == 200) {
-       photos = photoFromJson(response.body);
-         
-          print(
-              "Successed write ===>>> getPhotos > ${photos.toJson().toString()}");
+      photos = photoFromJson(response.body);
 
-          print("getPhotos: ${response.statusCode}");
+      print("Successed write ===>>> getPhotos > ${photos.toJson().toString()}");
+
+      print("getPhotos: ${response.statusCode}");
       print('Photos успешно отправлены на сервер');
     } else {
       print('Ошибка при отправке Photo на сервер');
     }
-    }catch(e) {
-      print("exeptionPhoto: ${e}");
-    }
-    
+  } catch (e) {
+    print("exeptionPhoto: ${e}");
   }
+}
